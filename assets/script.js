538f01b6c5500610fec8749644ff4e078ecd5814
Snake <chaldrich24@gmail.com>
2021-09-22 19:28:58 -0400

    add partial local storage functionality and fix layout issues

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,6 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm A"));
-
+var taskList = [];
 console.log($(".container"));
-
-
 
 $(".task-holder").on("click", ".task-hour", function() {
     $(this).removeClass("task-hour");
@@ -20,19 +18,54 @@ $(".task-holder").on("click", ".task-hour", function() {
 });
 
 $(".task-holder").on("blur", "textarea", function() {
-
     var text = $(this).val();
     var classList = $(this).attr("class").trim();
     var classArr = classList.split(" ");
 
+    var newDiv = $("<div>");
+    newDiv.addClass(classArr);
+    newDiv.addClass("task-hour")
+    newDiv.append("<p class='task-content'>" + text + "</p>");
+
     // delete textarea content and add div
-    $(this).replaceWith("<div class='task-hour'></div>");
-
-    // add back old classes to new div
-    $(".task-hour").addClass(classArr);
-
-    $(".task-hour").append("<p>");
-    $(".task-hour p").text(text);
-    $(".task-hour p").addClass("task-content");
+    $(this).replaceWith(newDiv.get(0));
+    
    
 });
+
+var saveTask = function(event) {
+    var task = $(".task-hour p").text();
+    var taskHour = event.target.parentNode.id;
+    var replace = false;
+    
+    var taskObj = {"task": task, "taskHour": taskHour};
+    
+    for (i = 0; i < taskList.length; i++) {
+        if (taskList[i].taskHour === taskObj.taskHour) {
+            taskList.splice(i, 1);
+            taskList.push(taskObj);
+            replace = true;
+        }
+    }
+
+    if (!replace) {
+        taskList.push(taskObj);
+    }
+    
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+};
+
+var loadTasks = function() {
+    taskList = JSON.parse(localStorage.getItem("tasks"));
+    if (!taskList) {
+        return false;
+    }
+    for (i = 0; i < taskList.length; i++) {
+        var taskEl = $("#" + taskList[i].taskHour + " .task-hour p");
+        taskEl.text(taskList[i].task)
+    }
+};
+
+
+
+$(".save-task").on("click", saveTask);
